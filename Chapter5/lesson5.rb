bd6d99566c2f897acfae0ce9f0bf133bd5ed2b15
EC2 Default User <ec2-user@ip-172-31-2-45.ap-northeast-1.compute.internal>
2023-04-19 06:36:41 +0000

    [Finish] 2-5

---
+++ Chapter5/lesson5.rb
@@ -0,0 +1,9 @@
+total_price = 100
+
+if total_price > 100
+  puts "Bought oranges, and you have some money yet"
+elsif total_price == 100
+  puts "You bought oranges"
+else
+  puts "You can't buy orange"
+end
