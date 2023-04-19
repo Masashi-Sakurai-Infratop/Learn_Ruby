d54a5a8b6b33721ea1182a59b7f902860bdb950a
EC2 Default User <ec2-user@ip-172-31-2-45.ap-northeast-1.compute.internal>
2023-04-19 21:33:21 +0000

    [Finish] 2-9

---
+++ Chapter9/class.rb
@@ -0,0 +1,31 @@
+class Math
+  x = 0
+  y = 0
+  z = 0
+
+  def set(x,y,z)
+    self.x = x
+    self.y = y
+    self.z = z
+  end
+
+  def change(multiple)
+    self.x *= multiple
+  end
+
+  def show()
+    puts self.x
+    puts self.y
+    puts self.z
+  end
+
+  def self.delete()
+    self.x = 0
+    self.y = 0
+    self.z = 0
+  end
+end
+
+math = Math.new()
+
+math.set(1, 2, 3)
